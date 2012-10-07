2bc8bcc741b9b3f95f0b2a2fbc6a99d536bb36f8
iRGBit <birgit@moddr.net>
2012-10-07 20:23:20 +0200

    test

---
+++ Demo/src/nl/sense/demo/Gmaps.java
@@ -0,0 +1,30 @@
+package nl.sense.demo;
+
+import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapView;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+import java.util.List;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import com.google.android.maps.GeoPoint;
+import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapView;
+import com.google.android.maps.Overlay;
+import com.google.android.maps.OverlayItem;
+
+
+public class Gmaps extends MapActivity{
+	public Gmaps() {
+	 
+	 @Override
+	 protected boolean isRouteDisplayed() {
+	     return false;
+	 }
+		// TODO Auto-generated constructor stub
+	}
+
+}
