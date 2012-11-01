8233676d7da5366ca4483b23c793f425bae7fb79
Pim Nijdam <pim.n@xs4all.nl>
2012-11-01 14:04:34 +0100

    Using in-development library.
    Using commit 851b95619f8535ae9d4cf739c4907d39077d7858 of the sense
    library.

--- Demo/src/nl/sense/demo/SenseActivity.java
+++ Demo/src/nl/sense/demo/SenseActivity.java
@@ -1,88 +1,36 @@
 package nl.sense.demo;
 
+import org.json.JSONArray;
+
 import android.os.Bundle;
-import android.os.IBinder;
-import android.os.RemoteException;
 import android.app.Activity;
 import android.content.BroadcastReceiver;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.content.ServiceConnection;
 import android.util.Log;
 import android.view.Menu;
 import nl.sense.demo.R;
 
+import nl.sense_os.platform.SensePlatform;
+import nl.sense_os.platform.SenseRemoteException;
+import nl.sense_os.platform.ServiceConnectionEventHandler;
 //import bunch of sense library stuff
 import nl.sense_os.service.ISenseService;
-import nl.sense_os.service.ISenseServiceCallback;
 import nl.sense_os.service.SenseService;
-import nl.sense_os.service.commonsense.SenseApi;
-import nl.sense_os.service.commonsense.SensorRegistrator;
 import nl.sense_os.service.constants.SensePrefs;
 import nl.sense_os.service.constants.SensePrefs.Main.Ambience;
 import nl.sense_os.service.constants.SensePrefs.Main.Location;
-import nl.sense_os.service.constants.SensorData.DataPoint;
 
-public class SenseActivity extends Activity {
+public class SenseActivity extends Activity implements ServiceConnectionEventHandler {
 
+	private static final String TAG = "SenseDemo";
+	private SensePlatform sensePlatform;
 	
 	/**
-	 * Service stub for callbacks from the Sense service.
-	 */
-	private class SenseCallback extends ISenseServiceCallback.Stub {
-
-		@Override
-		public void onChangeLoginResult(int result) throws RemoteException {
-			Log.v(TAG, "Login changed with result " + result);
-			sendData();
-		}
-
-		@Override
-		public void onRegisterResult(int result) throws RemoteException {
-			// not used
-		}
-
-		@Override
-		public void statusReport(final int status) {
-			// Log.v(TAG, "Received status report from Sense Platform service...");
-		}
-	}
-
-	/**
-	 * Service connection to handle connection with the Sense service. Manages the
-	 * <code>service</code> field when the service is connected or disconnected.
-	 */
-	private class SenseServiceConn implements ServiceConnection {
-
-		@Override
-		public void onServiceConnected(ComponentName className, IBinder binder) {
-			Log.v(TAG, "Bound to Sense Platform service...");
-
-			service = ISenseService.Stub.asInterface(binder);
-			try {
-				service.getStatus(callback);
-				setupSense();
-			} catch (final RemoteException e) {
-				Log.e(TAG, "Error checking service status after binding. ", e);
-			}
-		}
-
-		@Override
-		public void onServiceDisconnected(ComponentName className) {
-			Log.v(TAG, "Sense Platform service disconnected...");
-
-			/* this is not called when the service is stopped, only when it is suddenly killed! */
-			service = null;
-			isServiceBound = false;
-			checkServiceStatus();
-		}
-	}
-
-	/**
-	 * Receiver for broadcast events from the Sense Service, e.g. when the status of the service
-	 * changes.
+	 * Receiver for broadcast events from the Sense Service, e.g. when the
+	 * status of the service changes.
 	 */
 	private class SenseServiceListener extends BroadcastReceiver {
 
@@ -90,57 +38,7 @@ public class SenseActivity extends Activity {
 		public void onReceive(Context context, Intent intent) {
 		}
 	}
-
-	private static final String TAG = "SenseDemo";
-
-	private final ISenseServiceCallback callback = new SenseCallback();
-	private boolean isServiceBound;
-	private ISenseService service;
-	private final ServiceConnection serviceConn = new SenseServiceConn();
-	private final SenseServiceListener serviceListener = new SenseServiceListener();
-	private final SensorRegistrator sensorRegistrator = new SensorRegistrator(this) {
-		
-		@Override
-		public boolean verifySensorIds(String deviceType, String deviceUuid) {
-			return false;
-		}
-	};
-
-	/**
-	 * Binds to the Sense Service, creating it if necessary.
-	 */
-	private void bindToSenseService() {
-		// start the service if it was not running already
-		if (!isServiceBound) {
-			Log.v(TAG, "Try to bind to Sense Platform service");
-			final Intent serviceIntent = new Intent(getString(R.string.action_sense_service));
-			
-			isServiceBound = bindService(serviceIntent, serviceConn, BIND_AUTO_CREATE);
-			Log.v(TAG, "Result: " + isServiceBound);
-		} else {
-			// already bound
-		}
-	}
-
-	/**
-	 * Calls {@link ISenseService#getStatus(ISenseServiceCallback)} on the service. This will
-	 * generate a callback that updates the buttons ToggleButtons showing the service's state.
-	 */
-	private void checkServiceStatus() {
-		Log.v(TAG, "Checking service status..");
-
-		if (null != service) {
-			try {
-				// request status report
-				service.getStatus(callback);
-			} catch (final RemoteException e) {
-				Log.e(TAG, "Error checking service status. ", e);
-			}
-		} else {
-			Log.v(TAG, "Not bound to Sense Platform service! Assume it's not running...");
-		}
-	}
-
+	private SenseServiceListener senseListener = new SenseServiceListener();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -160,25 +58,17 @@ public class SenseActivity extends Activity {
 		super.onStart();
 
 		// bind to service as soon as possible
-		bindToSenseService();
+		sensePlatform = new SensePlatform(getApplicationContext(), this);
 
 		// register receiver for updates
 		IntentFilter filter = new IntentFilter(SenseService.ACTION_SERVICE_BROADCAST);
-		registerReceiver(serviceListener, filter);
+		registerReceiver(senseListener, filter);
 
-		checkServiceStatus();
 	}
 
 	@Override
 	protected void onStop() {
 		// Log.v(TAG, "onStop");
-
-		// unregister service state listener
-		try {
-			unregisterReceiver(serviceListener);
-		} catch (IllegalArgumentException e) {
-			// listener was not registered
-		}
 
 		super.onStop();
 	}
@@ -186,7 +76,8 @@ public class SenseActivity extends Activity {
 	private void setupSense() {
 		Log.v(TAG, "setupSense()");
 		try {
-			service.changeLogin("irgbit", SenseApi.hashPassword("soundcrowd"), callback);
+			ISenseService service = sensePlatform.service();
+			sensePlatform.login("irgbit", "soundcrowd");
 
 			//turn off specific sensors
 			service.setPrefBool(Ambience.LIGHT, false);
@@ -232,22 +123,36 @@ public class SenseActivity extends Activity {
 		String description = name;
 		//the value to be sent, in json format
 		String value = "{\"latitude\":\"51.903469\",\"longitude\":\"4.459865\",\"comment\":\"What a nice quiet place!\"}"; //json value
-		//register the sensor
-		sensorRegistrator.checkSensor(name, displayName, dataType, description, value, null, null);
+		
+		//send
 		long timestamp = System.currentTimeMillis();
-
-		// send data point
-		String action = getString(nl.sense_os.service.R.string.action_sense_new_data);
-		Intent intent = new Intent(action);
-		intent.putExtra(DataPoint.SENSOR_NAME, name);
-		intent.putExtra(DataPoint.DISPLAY_NAME, displayName);
-		intent.putExtra(DataPoint.SENSOR_DESCRIPTION, description);
-		intent.putExtra(DataPoint.DATA_TYPE, dataType);
-		intent.putExtra(DataPoint.VALUE, value);
-		intent.putExtra(DataPoint.TIMESTAMP, timestamp);
-		startService(intent);
-		Log.v(TAG, "Send data point: " + value);
+		try {
+			sensePlatform.addDataPoint(name, displayName, description, dataType, value, timestamp);
+		} catch (SenseRemoteException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	/** An example of how to receive data
+	 * 
+	 */
+	void getData() {
+		try {
+			JSONArray data = sensePlatform.getData("noise", true, 10);
+		} catch (SenseRemoteException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
+	@Override
+	public void onServiceConnected(ComponentName className,
+			ISenseService service) {
+		setupSense();
+	}
 
+	@Override
+	public void onServiceDisconnected(ComponentName className) {
+	}
 }
